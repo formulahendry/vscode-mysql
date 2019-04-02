28335d3bff564bc9cae347218e5bfdc77cfb3c8d
Álvaro González <varogonz95@gmail.com>
2019-04-02 08:09:16 -0600

    Escape database and table names (#97)
    Use escaped literals in DB object names. 
    For example, having a database with name 'my-database' will cause SQL Syntax Error, because of character '-'.

--- src/model/tableNode.ts
+++ src/model/tableNode.ts
@@ -47,7 +47,7 @@ export class TableNode implements INode {
 
     public async selectTop1000() {
         AppInsightsClient.sendEvent("selectTop1000");
-        const sql = `SELECT * FROM ${this.database}.${this.table} LIMIT 1000;`;
+        const sql = `SELECT * FROM \`${this.database}\`.\`${this.table}\` LIMIT 1000;`;
         Utility.createSQLTextDocument(sql);
 
         const connection = {
